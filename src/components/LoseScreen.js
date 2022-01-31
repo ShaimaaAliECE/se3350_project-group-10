f4058fd44981c8abb268d4920f69679b4c753457
Sashintha Tennakoon <34927075+sashintha@users.noreply.github.com>
2022-01-31 18:36:36 -0500

    setup lose screen

--- src/components/LoseScreen.js
+++ src/components/LoseScreen.js
@@ -1,8 +1,95 @@
 import React from 'react';
+import {makeStyles} from  "@material-ui/core";
+import Button from '@mui/material/Button';
+
+const useStyles = makeStyles((theme) => ({
+  container:{
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    height:'100vh',
+    backgroundColor:'lightGrey',
+  },
+
+  content:{
+    borderStyle:'solid',
+    borderColor:'black',
+    borderRadius:'20px',
+    width: '800px',
+    height: '500px',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+  },
+
+  title:{
+    textAlign:'center',
+    fontSize:'50px',
+  },
+
+  buttonSection:{
+    display:'flex',
+    textAlign:'center',
+    justifyContent:'center',
+    height:'200px',
+    flexDirection:'column',
+    padding:'100px',
+  },
+
+  buttons:{
+    padding:'20px',
+  },
+
+  })); 
 
 export default function LoseScreen() {
-  return (
-    <div></div>
 
+  const style = useStyles();
+
+  return (
+    <div className={style.container}>
+      <div className={style.content}>
+        <div className={style.title}>Nice Try!</div>
+
+        <div className={style.buttonSection}>
+
+          <div className={style.buttons}>
+            <Button 
+            variant="contained" 
+            className={style.button}
+            sx={{
+              backgroundColor:'#3D3D3D',
+              height:'50px',
+              width:'250px',
+              
+            }}
+            >Restart Level</Button>
+          </div>
+
+          <div className={style.buttons}>
+          <Button 
+            variant="contained" 
+            className={style.button}
+            sx={{
+              backgroundColor:'#3D3D3D',
+              height:'50px',
+              width:'250px',
+            }}
+            >Quit Level</Button>
+          </div>
+          
+          <div className={style.buttons}>
+          <Button 
+            variant="contained" 
+            className={style.button}
+            sx={{
+              backgroundColor:'#3D3D3D',
+              height:'50px',
+              width:'250px',
+            }}
+            >Switch Algorithms</Button>
+          </div>
+          
+        </div>
+      </div>
+    </div>
   );
 }
