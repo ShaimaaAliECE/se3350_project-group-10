fa83ebf36616c5b69050d0e9ddae58de714891d8
Sashintha Tennakoon <34927075+sashintha@users.noreply.github.com>
2022-01-31 16:15:33 -0500

    setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
+import LoseScreen from './components/LoseScreen.js'
 
 function App() {
   return (
-    <div>
-      Hello World!
-    </div>
+    <LoseScreen/>
   );
 }
 
---
+++ src/components/LoseScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function LoseScreen() {
+  return (
+    <div></div>
+
+  );
+}
