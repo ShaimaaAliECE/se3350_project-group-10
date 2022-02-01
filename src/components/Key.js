92629813562f91e944a088e2f842a5783bd2edf8
luxscious <ggerges019@gmail.com>
2022-02-01 12:21:52 -0500

    Create Key.js

---
+++ src/components/Key.js
@@ -0,0 +1,15 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  container: {},
+}));
+function Key(props) {
+  const styles = useStyles();
+  return (
+    <div>
+      <div>pinao stuff</div>
+    </div>
+  );
+}
+
+export default Key;
