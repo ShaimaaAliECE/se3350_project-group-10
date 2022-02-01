857d7ec33866139108ec68f7b0dc8952696ca625
ppate343 <ppate343@uwo.ca>
2022-02-01 12:26:24 -0500

    created sheet

---
+++ src/components/Sheet.js
@@ -0,0 +1,82 @@
+
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import { makeStyles } from "@material-ui/core";
+
+
+
+// add scroll css to container (filled with multiple  groups of lines)
+const useStyles = makeStyles((theme) => ({
+    container: {
+        flex: 'auto',
+        justifyContent: 'space-between',
+        background: 'white',
+        boxShadow: '0 3px 5px 2px rgba(0, 0, 0, 0.3)',
+        color: 'white',
+        height: 500,
+        width: 980,
+        marginTop: 80,
+        marginLeft: 150,
+        overflow:'scroll',
+
+    },
+    lines: {
+        borderBottom: 15,
+        color: "black",
+
+    },
+    lineRow: {
+      
+        flex: 'auto',
+        marginTop: 50,
+    },
+
+    
+}));
+
+
+
+
+
+//creating function component sheet 
+//include Lines as component 
+//Lines component should include state (black or white)
+//state of lines set to white originally 
+// next group of lines state changes to black once player submits correct row of inputs
+function Sheet() {
+    const style = useStyles();
+    return (
+        <div className={style.container}>
+            <Lines />
+            <Lines />
+            <Lines />
+            <Lines />
+            <Lines />
+            <Lines />
+            <Lines />
+            <Lines />
+        </div>
+    );
+}
+
+//creating function component lines 
+function Lines() {
+    const style = useStyles();
+
+    return (
+        <div className={style.lineRow}>
+            <hr className={style.lines}></hr>
+            <hr className={style.lines}></hr>
+            <hr className={style.lines}></hr>
+            <hr className={style.lines}></hr>
+            <hr className={style.lines}></hr>
+        </div>
+
+
+
+    )
+}
+
+
+export default Sheet; 
