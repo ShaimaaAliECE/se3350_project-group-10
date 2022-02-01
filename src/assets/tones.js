4cd1581906af30b689753c01b26a93255281df2d
luxscious <ggerges019@gmail.com>
2022-02-01 14:29:20 -0500

    Merge with Piano branch

---
+++ src/assets/tones.js
@@ -0,0 +1,28 @@
+import * as Tone from "tone";
+
+const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "B"];
+//Where notes is # of possible notes, currentNote is index of current Note
+function playSound(notes, currentNote) {
+  const synth = new Tone.Synth().toDestination();
+  let octave;
+  let currentNoteStandard;
+  let note;
+  //Based on how many notes, start at a different octave
+  //notes /12 gives u # of octaves needed
+  if (notes / 12 < 2) {
+    octave = 4;
+  } else if (notes / 12 === 3) {
+    octave = 3;
+  } else {
+    octave = 2;
+  }
+
+  let noteLevel = currentNote / 12;
+  currentNoteStandard = currentNote - 12 * noteLevel;
+  //for every note level increment octave, if note level is 1, add 1 to octave
+  octave += noteLevel;
+  note = notes[currentNoteStandard] + "" + octave;
+  synth.triggerAttackRelease(note, "8n");
+}
+
+export default playSound;
