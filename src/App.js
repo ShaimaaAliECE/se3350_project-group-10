55059ea1ff6bee00aaddc1fc352e4fb42b9b31e5
Sashintha Tennakoon <34927075+sashintha@users.noreply.github.com>
2022-02-03 18:14:49 -0500

    added routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import "./App.css";
+import {makeStyles} from  "@material-ui/core";
+import Button from '@mui/material/Button';
 import Piano from "./components/Piano";
+import LoseScreen from "./components/LoseScreen.js"
+import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-      }}
-    >
-      <Piano />
+    <div>
+      MAIN SCREEN
     </div>
   );
 }
