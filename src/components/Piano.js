0fb54b02bc11fda7c921ee6a6da9176c0ee50ffc
luxscious <ggerges019@gmail.com>
2022-02-03 18:04:45 -0500

    Update store initial state, Key, Piano to  update input state on piano press

--- src/components/Piano.js
+++ src/components/Piano.js
@@ -1,5 +1,7 @@
 import { makeStyles } from "@material-ui/core";
 import Key from "./Key";
+import { view } from "@risingstack/react-easy-state";
+
 const useStyles = makeStyles((theme) => ({
   piano: {
     display: "flex",
@@ -21,19 +23,18 @@ function RenderKeys(x) {
   for (let i = 0; i < x; i++) {
     temp.push(
       <div style={{}} key={i}>
-        <Key index={i} key={i} totalNotes={x} />
+        <Key index={i} totalNotes={x} />
       </div>
     );
   }
   return temp;
 }
-function Piano() {
+
+export default view(function Piano() {
   const styles = useStyles();
 
   //import store
   //get max# piano keys from store
   const pianoLength = 30;
   return <div className={styles.piano}>{RenderKeys(pianoLength)}</div>;
-}
-
-export default Piano;
+});
