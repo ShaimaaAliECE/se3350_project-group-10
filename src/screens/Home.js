113e01fe4346dd2c3cff272b82206190dca29344
jmutton2 <jmutton2@uwo.ca>
2022-02-03 18:39:20 -0500

    Some reformatting and random array generation

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,30 +1,50 @@
-import React from 'react';
-import {makeStyles} from  "@material-ui/core";
-import Button from '@mui/material/Button';
-import App from '../App';
+import React from "react";
+import { makeStyles } from "@material-ui/core";
+import Button from "@mui/material/Button";
+import App from "../App";
 import { Link } from "react-router-dom";
-import LoseScreen from "../components/LoseScreen.js"
+import LoseScreen from "../components/LoseScreen.js";
 
+const random = (min, max) => {
+  for (let i = 0; i < 10; i++) {
+    let num = Math.floor(Math.random() * (max - min) + min);
+    //state.sheet[0].push(num)
+  }
+};
 
-export default function Home(){
-    return(
-        <div>
-            HOME SCREEN
-            <div>
-                <Link to="/app">
-                    <Button 
-                    variant="contained" 
-                    sx={{
-                        backgroundColor:'#3D3D3D',
-                        height:'50px',
-                        width:'250px',
-                        
-                    }}
-                    >
-                        This goes to App.js
-                    </Button>
-                </Link>
-            </div>
-        </div>
-    )
+function handleClick(level) {
+  switch (level) {
+    case 1:
+      console.log("level 1");
+      break;
+    case 2:
+      console.log("level 2");
+      break;
+    case 3:
+      random(1 - 20);
+      break;
+  }
+}
+
+export default function Home() {
+  return (
+    <div>
+      HOME SCREEN
+      <div>
+        <Link to="/app">
+          <Button
+            variant="contained"
+            onClick={() => handleClick()}
+            sx={{
+              backgroundColor: "#3D3D3D",
+              height: "50px",
+              width: "250px",
+            }}
+          >
+            This goes to App.js
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
