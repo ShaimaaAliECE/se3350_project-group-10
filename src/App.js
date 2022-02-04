89527cf0d710b3ec8aa35474ad69079d91223024
luxscious <ggerges019@gmail.com>
2022-02-03 21:19:05 -0500

    edits

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-
 import "./App.css";
-import {makeStyles} from  "@material-ui/core";
-import Button from '@mui/material/Button';
+import { makeStyles } from "@material-ui/core";
+import Button from "@mui/material/Button";
 import Piano from "./components/Piano";
-import LoseScreen from "./components/LoseScreen.js"
+import LoseScreen from "./components/LoseScreen.js";
 import { Link } from "react-router-dom";
-
+import InputContainer from "./components/InputContainer";
 
 function App() {
   return (
     <div>
-      MAIN SCREEN
+      <InputContainer />
+      <Piano />
     </div>
   );
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -24,6 +24,7 @@ function handleClick(level) {
     case 3:
       random(1, 20);
       break;
+    default:
   }
 }
 
