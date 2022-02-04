1d3a4e4f4a990b9fce0e6830dfc83750633724e4
luxscious <57682940+luxscious@users.noreply.github.com>
2022-02-03 23:01:17 -0500

    merge sort algo added

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,6 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core";
 import Button from "@mui/material/Button";
-import App from "../App";
 import { Link } from "react-router-dom";
-import LoseScreen from "../components/LoseScreen.js";
 import state from "../store/Store";
 
 const random = (min, max) => {
@@ -12,8 +9,6 @@ const random = (min, max) => {
     state.sheet[0].push(num);
   }
 };
-
-// const mergsort function
 
 function handleClick(level) {
   switch (level) {
@@ -25,9 +20,64 @@ function handleClick(level) {
       break;
     case 3:
       random(1, 20);
-      // mergsortFunction(state.sheet[0])
+      mergeSort(state.sheet[0], 0, state.sheet[0].length - 1);
       break;
     default:
+  }
+}
+let step = 1;
+function merge(arr, start, mid, end) {
+  let start2 = mid + 1;
+  console.log("INCREMENT STEP");
+  step++;
+  // If the direct merge is already sorted
+  if (arr[mid] <= arr[start2]) {
+    return;
+  }
+
+  // Two pointers to maintain start
+  // of both arrays to merge
+  while (start <= mid && start2 <= end) {
+    // If element 1 is in right place
+    if (arr[start] <= arr[start2]) {
+      start++;
+    } else {
+      let value = arr[start2];
+      let index = start2;
+
+      // Shift all the elements between element 1
+      // element 2, right by 1.
+      while (index != start) {
+        arr[index] = arr[index - 1];
+        index--;
+      }
+      arr[start] = value;
+
+      // Update all the pointers
+      start++;
+      mid++;
+      start2++;
+    }
+  }
+}
+
+function mergeSort(arr, l, r) {
+  console.log(
+    "step",
+    step,
+    arr.slice(0, arr.length / step),
+    arr.slice(arr.length / step, arr.length)
+  );
+  if (l < r) {
+    // Same as (l + r) / 2, but avoids overflow
+    // for large l and r
+    let m = l + Math.floor((r - l) / 2);
+
+    // Sort first and second halves
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, r);
+
+    merge(arr, l, m, r);
   }
 }
 
