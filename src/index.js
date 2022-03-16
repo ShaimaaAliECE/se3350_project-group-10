14ed4713085441e331f91dfd8a7255c18164468b
luxscious <ggerges019@gmail.com>
2022-03-16 17:57:09 -0400

    Update Git Ignore to ignore env, install firebase, Initialize Firebase

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./screens/Home.js";
 import Level from "./screens/Level.js";
 
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+const app = initializeApp(firebaseConfig);
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
