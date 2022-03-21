63765f00b7a7c2d77dcfc20220bac3517fbcaa6f
Nick <nicholasgin@gmail.com>
2022-03-21 18:18:15 -0400

    added drawer pull out

--- src/components/InstructionPopup.js
+++ src/components/InstructionPopup.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import { Drawer, makeStyles } from "@material-ui/core";
 import levelone from "../assets/level-one.json";
 import state from "../store/Store";
-import { view } from "@risingstack/react-easy-state";
+import { autoEffect, view } from "@risingstack/react-easy-state";
+import backBtn from "../assets/back.svg";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -11,7 +12,8 @@ const useStyles = makeStyles((theme) => ({
     left: "2%",
     borderRadius: 30,
     width: 375,
-    minHeight: 150,
+    // minHeight: 150,
+    height: "100vh",
     backgroundColor: "rgba(242,242,242,0.9)",
     margin: "auto",
     zIndex: 6,
@@ -26,22 +28,29 @@ const useStyles = makeStyles((theme) => ({
 
   content: {
     textAlign: "justified",
-    backgroundColor: "transparent",
+    backgroundColor: "rgba(242,242,242,0.9)",
     fontFamily: "Raleway",
     fontSize: "19px",
     color: "#1a1a1a",
+    width: "400px",
+    // height: "400px",
+    marginTop: "80%",
   },
 
   buttonDrawer: {
     position: "absolute",
-    left: "5%",
-    bottom: "25%",
+    right: "1%",
+    bottom: "23%",
   },
 
   showButton: {
     fontFamily: "Raleway",
-    fontSize: "15px",
+    fontSize: "20px",
     borderRadius: 30,
+  },
+
+  Drawer: {
+    backgroundColor: "rgba(0,0,0,0)",
   },
 }));
 
@@ -57,38 +66,39 @@ function InstructionPopup() {
           <button
             className={classes.showButton}
             onClick={() => {
-              setDrawer(true);
+              setDrawer(!drawer);
             }}
           >
-            Instruction Hide
+            {" "}
+            {drawer ? "Hide Instructions" : "Show Instructions"}
           </button>
-
           <Drawer
-            variant="temporary"
+            className={classes.Drawer}
+            // style={{
+            //   background: "black",
+            // }}
+            variant="persistent"
             anchor={"left"}
             open={drawer}
             onClose={() => {
               setDrawer(false);
             }}
-            ModalProps={{keepMounted: true}}
           >
-            {
-              <div className={classes.container}>
-                <div className={classes.detailsContainer}>
-                  <div className={classes.content}>
-                    <link
-                      rel="stylesheet"
-                      type="text/css"
-                      href="//fonts.googleapis.com/css?family=Raleway"
-                    />
-                    {state.instruct === 0
-                      ? "Split the array in half as evenly as possible. Take the floor function of the length/2 to correctly divide the array. Start by entering the values of the left sub-array."
-                      : levelone.map((levelone) => levelone[state.instruct])}
-                  </div>
-                </div>
-              </div>
-            }
+            {/* <div className={classes.container}>
+              <div className={classes.detailsContainer}></div>
+            </div> */}
+            <div className={classes.content}>
+              <link
+                rel="stylesheet"
+                type="text/css"
+                href="//fonts.googleapis.com/css?family=Raleway"
+              />
+              {state.instruct === 0
+                ? "Split the array in half as evenly as possible. Take the floor function of the length/2 to correctly divide the array. Start by entering the values of the left sub-array."
+                : levelone.map((levelone) => levelone[state.instruct])}
+            </div>
           </Drawer>
+          {/* </div> */}
         </>
       </div>
     </>
